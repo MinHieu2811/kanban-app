85442f11a209f2927c1da9d6988f0522638aa2f5
MinHieu2811 <cuunhatnhat51@gmail.com>
2022-11-20 22:34:54 +0700

    config layout and models layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { CacheProvider } from '@emotion/react'
+import { ThemeProvider } from '@mui/material'
+import { theme, createEmotionCache } from '@/utils/index'
+import { EmptyLayout } from 'components/layout'
+import { AppPropsWithLayout } from '@/models/layout'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache()
+
+function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache }: AppPropsWithLayout) {
+  const Layout = Component.Layout ?? EmptyLayout
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
+export default MyApp
